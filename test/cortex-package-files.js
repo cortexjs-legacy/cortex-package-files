ab95342b49760131f4090842dddea2934dd36397
kaelzhang <i@kael.me>
2014-06-16 18:15:38 +0800

    updates test cases; #1

--- test/cortex-package-files.js
+++ test/cortex-package-files.js
@@ -5,6 +5,7 @@ var pf = require('../');
 var packages = require('cortex-playground').packages;
 var cortex_json = require('read-cortex-json');
 var fss = require('fs-sync');
+var node_path = require('path');
 
 function exists (path, files) {
   return !!~files.indexOf(path);
@@ -48,11 +49,28 @@ p2.copy(function (err, dir) {
   }
   cortex_json.read(dir, function (err, json) {
     assert.equal(err, null, 'fail to read cortex.json');
+    
+    var node_module_files = [
+      'node_modules/package.json',
+      'node_modules/cortex.json',
+      'node_modules/a/c/d/f/README.md'
+    ];
+
+    // #1
+    node_module_files.forEach(function (path) {
+      var file = node_path.join(dir, path);
+      fss.write(file, '');
+    });
+
     pf({
       cwd: dir,
       pkg: json,
       more: true
     }, function (err, files) { console.log('p2 files', files)
+      node_module_files.forEach(function (file) {
+        assert.ok(noexists(file, files), 'should exclude node_modules');
+      });
+
       assert.equal(err, null, 'fail to get cortex files');
       assert.ok(exists('.cortexignore', files), 'should include dot file');
       // has no pkg.css
